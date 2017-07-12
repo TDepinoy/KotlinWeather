a672d859d40883218b1ecc2db9ca1df7f0f7ed47
Depinoy Thomas <tdepinoy.externe@pagesjaunes.fr>
2017-07-12 11:16:38 +0200

    Improve DI implementation with Kodein

--- app/src/main/java/com/tdepinoy/kotlinweather/forecast/ForecastPresenter.kt
+++ app/src/main/java/com/tdepinoy/kotlinweather/forecast/ForecastPresenter.kt
@@ -1,15 +1,12 @@
 package com.tdepinoy.kotlinweather.forecast
 
-import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.instance
 import com.tdepinoy.kotlinweather.core.api.WeatherApi
 import com.tdepinoy.kotlinweather.core.model.Forecast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-class ForecastPresenter(val kodein: Kodein, val view: ForecastContract.View) : ForecastContract.Presenter {
+class ForecastPresenter(val view: ForecastContract.View, val weatherApi: WeatherApi) : ForecastContract.Presenter {
 
-    val weatherApi: WeatherApi = kodein.instance()
 
     override fun fetchForecast() {
         weatherApi.dailyForecast("94043")
